96f18b989605997cca6032666264fdff183cc5dc
denisdiaconu <denisdiaconu117@yahoo.com>
2021-11-25 16:26:38 +0100

    Fix linters errors

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,6 +1,4 @@
 class SplashController < ApplicationController
-    skip_before_action :authenticate_user!, only: :index
-    def index
-    
-    end
+  skip_before_action :authenticate_user!, only: :index
+  def index; end
 end
