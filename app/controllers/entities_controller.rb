974f0306b363bc91e1cda40a9296073551252172
denisdiaconu <denisdiaconu117@yahoo.com>
2021-11-25 17:33:21 +0100

    Add path to destroy

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -49,7 +49,7 @@ class EntitiesController < ApplicationController
   def destroy
     @entity.destroy
     respond_to do |format|
-      format.html { redirect_to groups_url, notice: 'Entity was successfully destroyed.' }
+      format.html { redirect_to groups_path, notice: 'Entity was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
